7a81363293e575b207a847fa72d7c4a8793bac54
Alisa <alisabox@mail.ru>
2021-10-04 15:06:52 +0900

    add components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main/main';
+
+type AppScreenProps = {
+  cardsNumber: number;
+}
+
+function App({cardsNumber}: AppScreenProps): JSX.Element {
+  return <MainScreen cardsNumber={cardsNumber}/>;
 }
 
 export default App;
